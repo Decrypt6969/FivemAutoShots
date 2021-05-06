b11977a02ad2af1713680ffdc6f23993723ad44e
Decrypt <33306718+Decrypt6969@users.noreply.github.com>
2021-05-06 02:26:30 -0300

    asfas
    fasf

--- server.lua
+++ server.lua
@@ -1,7 +1,1 @@
-RegisterServerEvent('TakeScreenShotSRK')
-AddEventHandler('TakeScreenShotSRK',function(name)
-    exports['screenshot-basic']:requestClientScreenshot(source, {
-        fileName = 'cache/'..name..'.jpg'
-    }, function(err, data)
-    end)
-end)
+teste()
